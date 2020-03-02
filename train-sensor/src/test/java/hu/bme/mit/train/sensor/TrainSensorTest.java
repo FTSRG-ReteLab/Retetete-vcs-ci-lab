5cb33008ddace31d8857d65e9a3bc90091bb8f57
Sa2x <bsanyi1998@gmail.com>
2020-03-02 16:48:46 +0100

    finish him

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -5,18 +5,25 @@ import org.junit.Before;
 import org.junit.Test;
 import static org.mockito.Mockito.*;
 
+import hu.bme.mit.train.controller.TrainControllerImpl;
 import hu.bme.mit.train.interfaces.TrainController;
 import hu.bme.mit.train.interfaces.TrainSensor;
 import hu.bme.mit.train.interfaces.TrainUser;
-import hu.bme.mit.train.system.TrainSystem;
+import hu.bme.mit.train.sensor.TrainSensorImpl;
+import hu.bme.mit.train.user.TrainUserImpl;
+
+
 
 public class TrainSensorTest {
-	TrainSensor sensor;
+	 private TrainController controller ;
+	 private TrainUser user;
+	 private TrainSensor sensor;
     @Before
     public void before() {
-        TrainSystem system = new TrainSystem();
-    	sensor = system.getSensor();
-	sensor.overrideSpeedLimit(50);
+			controller= new TrainControllerImpl();
+			 user = new TrainUserImpl(controller);
+			 sensor = new TrainSensorImpl(controller, user);
+			sensor.overrideSpeedLimit(50);
 }
 
     @Test
