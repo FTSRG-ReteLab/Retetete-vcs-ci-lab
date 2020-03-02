3f3e71844a277d69686cff58592d169dac0d0c9d
Sa2x <bsanyi1998@gmail.com>
2020-03-02 16:24:13 +0100

    pls

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -5,6 +5,11 @@ import org.junit.Before;
 import org.junit.Test;
 import static org.mockito.Mockito.*;
 
+import hu.bme.mit.train.interfaces.TrainController;
+import hu.bme.mit.train.interfaces.TrainSensor;
+import hu.bme.mit.train.interfaces.TrainUser;
+import hu.bme.mit.train.system.TrainSystem;
+
 public class TrainSensorTest {
 	TrainSensor sensor;
     @Before
